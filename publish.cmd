544be84814ef51e2aa55e579c25bf5c97d70fc2d
Wolfgang <Wolfgang@ProgrammingWithWolfgang.com>
2018-08-25 23:35:56 +0200

    first commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=wolfgangbot-b3fd-Web-Deploy.pubxml -p:Password=AQwEbGlH72JPR98FMm1oEj7TlaKPFy0ruDWKoxpnMf1D1ykRrMATz0CrZmkY
+
